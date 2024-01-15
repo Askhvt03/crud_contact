811932db3e7c091b57480e141d9c4f344fda6626
Raushan <raushan.muslimova2003@gmail.com>
2024-01-15 21:48:02 +0600

    create contact crud project with postgresql and mongodb

---
+++ src/main/java/com/example/crudcontact/CruDcontactApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudcontact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruDcontactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CruDcontactApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/crudcontact/CruDcontactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudcontact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruDcontactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
